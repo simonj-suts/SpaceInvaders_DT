514520a0679722d7ac4f099b247d32f9636b76da
Joey <44018390+joeytzy@users.noreply.github.com>
2020-06-01 15:15:01 +0800

    Add files via upload

---
+++ SpaceInvaders_DT-vs/Asteroid.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    class Asteroid
+    {
+        #region Private fields
+        Size size;              // size
+        int speed, x, y;        // speed and position
+        #endregion
+
+        #region Private fields
+        public PictureBox Sprite { get; private set; }
+        #endregion
+
+        #region Constructors and factory methods
+        public Asteroid(Size size, int speed)
+        {
+            this.size = size;
+            this.speed = speed;
+
+            Sprite = new PictureBox
+            {
+                Size = size,
+                BackColor = Color.Transparent,
+                Image = Properties.Resources.asteroid,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            Sprite.BringToFront();
+        }
+        #endregion
+        
+        #region Public methods
+        public void SetLocation(int x, int y)
+        {
+            this.x = x - size.Width / 2;
+            this.y = y - size.Width / 2;
+            Sprite.Location = new Point(this.x, this.y);
+        }
+
+        public void Move()
+        {
+            y += speed;
+            Sprite.Top += speed;
+        }
+
+        public bool IsOutOfScreen(int screenH)
+        {
+            return y > screenH;
+        }
+
+        public bool IsCollision(Rectangle rect)
+        {
+            return Sprite.Bounds.IntersectsWith(rect);
+        } 
+        #endregion
+    }
+}
---
+++ SpaceInvaders_DT-vs/Missle.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    class Missle
+    {
+        #region Private fields
+        Size size;
+        int speed, x, y;
+        int playerNo;
+        #endregion
+
+        #region Public fields
+        public PictureBox Sprite { get; private set; }
+        public int PlayerNo { get { return playerNo; } }
+        #endregion
+
+        #region Constructors and factory methods
+        public Missle(Size size, int speed, int playerNo)
+        {
+            this.size = size;
+            this.speed = speed;
+            this.playerNo = playerNo;
+
+            Sprite = new PictureBox
+            {
+                Tag = "missle",
+                Size = size,
+                BackColor = Color.Transparent,
+                Image = Properties.Resources.missle,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            Sprite.BringToFront();
+        }
+        #endregion
+
+        #region Public methods
+        public void SetLocation(int x, int y)
+        {
+            this.x = x - size.Width / 2;
+            this.y = y - size.Height / 2;
+            Sprite.Location = new Point(this.x, this.y);
+        }
+
+        public void Move()
+        {
+            y -= speed;
+            Sprite.Top -= speed;
+        }
+
+        public bool IsOutOfScreen(Player player)
+        {
+            return y < player.playerCoordinateY;
+        } 
+        #endregion
+    }
+
+}
