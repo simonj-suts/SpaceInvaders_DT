294aebb9892a2738f7c24b99558c61c28809cc30
Joey <44018390+joeytzy@users.noreply.github.com>
2020-06-09 21:58:37 +0800

    Update VersusTest.cs

--- SpaceInvaderTest/VersusTest.cs
+++ SpaceInvaderTest/VersusTest.cs
@@ -17,24 +17,17 @@ namespace SpaceInvaderTest
             Size missleSize = new Size(15, 45);
             int missleSpeed = 50;
 
-            List<Weapon> missles = new List<Weapon>();
-            List<Weapon> missles2 = new List<Weapon>();
-
             Player player1 = new Player(playerSize, numberOfPositions, numberOfLives, 1); // create player 1
-            player1.Reposition(1920, 1080, 1);
             Rectangle playerArea = new Rectangle((int)player1.X, (int)player1.Y, 75, 75);
 
             Player player2 = new Player(playerSize, numberOfPositions, numberOfLives, 3); // create player 2
-            player2.Reposition(1920, 1080, 3);
             Rectangle playerArea2 = new Rectangle((int)player2.X, (int)player2.Y, 75, 75);
 
             Weapon missle = player1.CreateWeapon(missleSpeed, 1);
             Rectangle missle1Area = new Rectangle((int)missle.X, (int)missle.Y, 75, 75);
-            missles.Add(missle);
 
             Weapon missle2 = player2.CreateWeapon(missleSpeed, 3);
             Rectangle missle2Area = new Rectangle((int)missle2.X, (int)missle2.Y, 75, 75);
-            missles2.Add(missle2);
 
             bool missleHit = false;
             if (missle1Area.IntersectsWith(playerArea))
